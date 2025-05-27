e854999e4952b84e87a993cdf28a7690e7d9d64d
Shirin183 <rumayarumiya@gmail.com>
2025-05-27 10:44:59 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// // Находим элементы формы
+// const loginForm = document.getElementById('loginForm');
+// const usernameInput = document.getElementById('username');
+// const passwordInput = document.getElementById('password');
+// const submitButton = document.getElementById('submitButton');
+// const togglePassword = document.getElementById('togglePassword');
+// const errorMessage = document.getElementById('errorMessage');
+
+let head = document.getElementById(header)
